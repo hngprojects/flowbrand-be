8f33b7df4e366714b8b9fe09737d8146d69090cf
Prestige Nsien <prestigensien45@gmail.com>
2026-05-09 08:45:04 +0100

    fix: add console.log to randomNumber in generate-token.js

--- src/utils/generate-token.ts
+++ src/utils/generate-token.ts
@@ -5,5 +5,8 @@ export function generateSixDigitToken(): string {
   const max = 999999;
   const range = max - min + 1;
   const randomNumber = (crypto.randomBytes(3).readUIntBE(0, 3) % range) + min;
+
+  console.log(randomNumber)
   return randomNumber.toString();
+
 }
